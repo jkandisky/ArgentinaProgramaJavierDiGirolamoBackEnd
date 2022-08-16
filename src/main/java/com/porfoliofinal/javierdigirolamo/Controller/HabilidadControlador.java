18c2e1005651be281a863eeb52da9831e76e829c
jkandisky <jkandisky@hotmail.com>
2022-08-15 23:33:29 -0300

    make it better

--- src/main/java/com/porfoliofinal/javierdigirolamo/Controller/HabilidadControlador.java
+++ src/main/java/com/porfoliofinal/javierdigirolamo/Controller/HabilidadControlador.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 @RequestMapping("/habilidades")
 @CrossOrigin(origins = "https://javierdigirolamofront.web.app")
