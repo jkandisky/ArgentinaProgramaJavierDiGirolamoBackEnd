c7c514688ca6d17b0da6334c0d7380a60b99135f
Javier Di girolamo <94693968+jkandisky@users.noreply.github.com>
2022-08-19 15:51:24 -0300

    Cambios Menores

--- src/main/java/com/porfoliofinal/javierdigirolamo/Controller/CEducacion.java
+++ src/main/java/com/porfoliofinal/javierdigirolamo/Controller/CEducacion.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("edulab")
+@RequestMapping("/edulab")
 @CrossOrigin(origins = "https://javierdigirolamofront.web.app")
 public class CEducacion {
     @Autowired
