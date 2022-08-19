22cb3ee9b0b5d5f10589dd53a1f083351afebde3
Javier Di girolamo <94693968+jkandisky@users.noreply.github.com>
2022-08-19 15:52:07 -0300

    Cambios Menores

--- src/main/java/com/porfoliofinal/javierdigirolamo/Controller/CSobremi.java
+++ src/main/java/com/porfoliofinal/javierdigirolamo/Controller/CSobremi.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("soblab")
+@RequestMapping("/soblab")
 @CrossOrigin(origins = "https://javierdigirolamofront.web.app")
 public class CSobremi {
     @Autowired
